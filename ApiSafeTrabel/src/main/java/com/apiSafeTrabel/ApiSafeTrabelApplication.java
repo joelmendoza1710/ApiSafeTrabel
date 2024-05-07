6965366c55cad95f8b7a0fd0bf7af2958e23ad58
joelmendoza1710 <145950401+joelmendoza1710@users.noreply.github.com>
2024-05-07 16:48:46 -0500

    estructura lista

---
+++ ApiSafeTrabel/src/main/java/com/apiSafeTrabel/ApiSafeTrabelApplication.java
@@ -0,0 +1,13 @@
+package com.apiSafeTrabel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSafeTrabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSafeTrabelApplication.class, args);
+	}
+
+}
---
+++ ApiSafeTrabel/src/test/java/com/apiSafeTrabel/ApiSafeTrabelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiSafeTrabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSafeTrabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
